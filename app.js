9179cd8c95bb3db06754abf8259981ed063a88b4
wmiq84 <michaelwang3489@gmail.com>
2024-09-22 15:13:55 -0700

    Update initial files

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const path = require("node:path");
+const express = require('express');
+const path = require('node:path'); 
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+const app = express();
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.set('views', path.join(__dirname, 'views')); // for ejs
+app.set('view engine', 'ejs');
+
+app.listen(3000, () => {
+	console.log('Server is running on http://localhost:3000');
+});
